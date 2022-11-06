e51996d8d6ef70cfeece4f4f87e8fd148d368104
eunsun <eunsunkim13@gmail.com>
2022-11-06 20:49:15 +0900

    Add NHN cloud init script

--- init script/NHN.sh
+++ init script/NHN.sh
@@ -8,6 +8,7 @@ echo 'eunsun' | passwd --stdin eunsun
 cp -a /etc/sudoers /etc/sudoers.bak 
 echo "eunsun  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers 
 
+setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config 
 
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
@@ -16,3 +17,5 @@ systemctl restart sshd
 
 systemctl disable firewalld 
 systemctl stop firewalld
+
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtim
