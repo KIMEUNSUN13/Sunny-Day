ccad107e503201051fb8b299a4db6f71e82366a0
eskim <eunsunkim13@gmail.com>
2022-12-02 15:14:55 +0900

    Update before_reboot.sh backup dir name change reboot -> mgmt

--- ex/shell/reboot/before_reboot.sh
+++ ex/shell/reboot/before_reboot.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# Reboot 절차
-# 1. 프로세스 백업 필요 -> 리부트 후 올라오지 않는 프로세스 존재
-# 2. 라우트 테이블 백업 필요 -> 단순 명령어로 추가해둔 정책은 리부트 후에 사라짐
-# 3. 파일시스템 마운트 확인 필요 -> blkid가 /etc/fstab과 다른 경우 리부팅 시 오류
-
-####
-# 4. 사용자별 환경변수, 사용자별 프로세스
-# cut -f1,6 -d: /etc/passwd
-# .bashrc, .bash_profile
-  # alias
-# 루트
-# /etc/profile
-
-# find rm
-# find ./ -mtime -10 (현재 시간으로부터 10일전에 수정된 파일)
-# 지우는거
-###
-
-# 개선점이 많은 Reboot Script!
-# 1. before, after 스크립트에 중복 코드 존재
-# 2. 실행 시키기 위해 chmod u+x 필요
-# 3. 여러대의 서버의 경우 스크립트를 전달(scp) 후 실행(ssh user@ip 'command')
-#      3-1. 만약 스트립트에 변경사항이 있다면?
 
 ## (before) VM Reboot Process  ##
 
@@ -32,7 +9,7 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 
 DATE="$(/usr/bin/date +%Y-%m-%d)"
-WORK_DIR=/root/reboot/${DATE}
+WORK_DIR=/root/mgmt/${DATE}
 
 /usr/bin/mkdir -p ${WORK_DIR}
 
