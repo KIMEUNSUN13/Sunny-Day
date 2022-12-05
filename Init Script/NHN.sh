a2c21fc553a4cfee9d684d5bc1f06fdb47ca3896
eskim <eunsunkim13@gmail.com>
2022-12-05 15:11:24 +0900

    Update NHN.sh

--- Init Script/NHN.sh
+++ Init Script/NHN.sh
@@ -12,20 +12,19 @@ echo 'wasadm' | passwd --stdin wasadm
 
 cp -a /etc/sudoers /etc/sudoers.bak 
 {
-    # echo "%wheel  ALL=(ALL)       NOPASSWD: ALL"
     # echo "%wheel  ALL=(ALL)       ALL" 
+    # echo "#%wheel  ALL=(ALL)       NOPASSWD: ALL"
     echo "wasadm  ALL=(ALL)       ALL"
 }| tee -a /etc/sudoers 
 
-## wheel 그룹의 사용자만 su 사용 ##
 cp -a /etc/pam.d/su /etc/pam.d/su.bak 
 sed -i "s/#auth required pam_wheel.so use_uid/auth required	pam_wheel.so use_uid/g" /etc/pam.d/su
 
-## root 계정 원격 접속 제한 ## ## PermitRootLogin no ##
+# PermitRootLogin no
 cp -a /etc/securetty /etc/securetty.bak 
 sed -ri "s/pts\/[0-9]//g" /etc/securetty
 
-# authconfig 사용?
+# authconfig 사용??
 cp -a /etc/security/pwquality.conf /etc/security/pwquality.conf.bak
 {
     echo "lcredit=-1" # 영문 소문자
@@ -35,7 +34,6 @@ cp -a /etc/security/pwquality.conf /etc/security/pwquality.conf.bak
     echo "minlen=8"   # 최소 패스워드 길이 설정
 }| tee -a /etc/security/pwquality.conf
 
-## ssh 접속 시 비밀번호로 접속 ##
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 systemctl restart sshd
 
